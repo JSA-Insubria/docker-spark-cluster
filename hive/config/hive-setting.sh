964c276d7fde401e5d1c4f5ee19abe6abb73f6c3
Simone Bottoni <39499778+SimoneBottoni@users.noreply.github.com>
2020-01-13 13:00:24 +0100

    Source env

--- hive/config/hive-setting.sh
+++ hive/config/hive-setting.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+source .bashrc
+
+echo ">> mkdir /tmp"
 /home/hadoop/hadoop/bin/hadoop fs -mkdir /tmp
+echo ">> mkdir /user/hive/warehouse"
 /home/hadoop/hadoop/bin/hadoop fs -mkdir -p /user/hive/warehouse
+echo ">> chmod /tmp"
 /home/hadoop/hadoop/bin/hadoop fs -chmod g+w /tmp
+echo ">> chmod /user/hive/warehouse"
 /home/hadoop/hadoop/bin/hadoop fs -chmod g+w /user/hive/warehouse
-
-
-cd /home/hadoop/hive/bin/
-./schematool -initSchema -dbType derby
+echo ">> initSchema"
+/home/hadoop/hive/bin/schematool -initSchema -dbType derby
