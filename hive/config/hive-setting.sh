b8d0a70d695b0702ff09ef5d25eccaac23a3f5e8
Simone <simone@pclinux-simone.fritz.box>
2020-01-26 13:46:21 +0100

    Add Tez Support

--- hive/config/hive-setting.sh
+++ hive/config/hive-setting.sh
@@ -2,6 +2,19 @@
 
 source .bashrc
 
+
+hdfs dfsadmin -safemode leave
+echo ">> mkdir /apps"
+hdfs dfs -mkdir -p /apps/tez
+echo ">> cp /tez"
+hadoop fs -copyFromLocal /home/hadoop/tez/share/tez.tar.gz /apps/tez/
+echo ">> chmod /apps/tez"
+hadoop fs -chmod g+w /apps/tez
+echo ">> chmod /apps/tez/tez.tar.gz"
+hadoop fs -chmod g+w /apps/tez/tez.tar.gz
+
+
+
 echo ">> mkdir /tmp"
 /home/hadoop/hadoop/bin/hadoop fs -mkdir /tmp
 echo ">> mkdir /user/hive/warehouse"
